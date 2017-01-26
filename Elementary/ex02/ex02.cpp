2976533e99d2a57217a5286c6195fe19401d46c0
LukahS <lstrycker@mail.csuchico.edu>
2017-01-26 11:42:12 -0800

    Submitting solution for Example 2.

--- Elementary/ex02/ex02.cpp
+++ Elementary/ex02/ex02.cpp
@@ -1,1 +1,11 @@
 //Write a program that asks the user for her name and greets her with her name.
+#include<iostream>
+using namespace std;
+int main()
+{
+  string name;
+  cout << "What is your name?"<<endl;
+  cin >> name;
+  cout << "Hello, " << name << "!" <<endl;
+  return 0;
+}
