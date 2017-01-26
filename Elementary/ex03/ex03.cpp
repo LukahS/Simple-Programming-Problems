1daa0f3222c1d7761ec6a71f9692c241071388eb
LukahS <lstrycker@mail.csuchico.edu>
2017-01-26 11:51:07 -0800

    Solution for Example 3 Submitted in C++.

--- Elementary/ex03/ex03.cpp
+++ Elementary/ex03/ex03.cpp
@@ -1,1 +1,12 @@
 //Modify the previous program such that only the users Alice and Bob are greeted with their names.
+#include<iostream>
+using namespace std;
+int main()
+{
+  string name;
+  cout << "Please enter your name." << endl;
+  cin >> name;
+  if (name == "Alice" || name == "alice" || name == "Bob" || name == "bob")
+    cout << "Hello, " << name << "!" << endl;
+  return 0;
+}
